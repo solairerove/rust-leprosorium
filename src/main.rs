7d03dfa6f58940b27d0599af584b65b833383e67
solairerove <solairerove@gmail.com>
2025-08-18 18:35:00 +0700

    just classic math ops

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
-    let guess: u32 = "42".parse().expect("Not a number!");
-    println!("Guess is {}", guess);
+    let sum = 5 + 3;
+    println!("{}", sum);
+
+    let difference = 95.5 - 4.3;
+    println!("{}", difference);
+
+    let truncate = 5 / -3;
+    println!("{}", truncate);
+
+    let division = 56.7 / 33.2;
+    println!("{}", division);
 }
