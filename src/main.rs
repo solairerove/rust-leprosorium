12bedd59472fadfe7c5c3dd6b66f2ef98a326b8e
solairerove <solairerove@gmail.com>
2025-08-18 22:35:22 +0700

    arrays

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 fn main() {
-    let tup:(i32, f64, &str) = (500, 6.5, "1");
-    let (x, y, z) = tup;
-    println!("x: {}, y: {}, z: {}", x, y, z);
+    let a = [1, 2, 3, 4, 5];
+    println!("{:?}", a);
 
-    let five_hundred = tup.0;
-    let six_point_five = tup.1;
-    let one = tup.2;
-
-    println!("[0]: {five_hundred}, [1]: {six_point_five}, [2]: {one}")
+    let duplicate = [3; 5];
+    println!("{:?}", duplicate);
 }
