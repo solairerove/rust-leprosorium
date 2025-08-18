f0538dd71f51c5648e2394d29dd949d006dd97e5
solairerove <solairerove@gmail.com>
2025-08-18 17:23:29 +0700

    const and some nonsense data type, int32?

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,7 @@ fn main() {
     print!("The value of x is {x}\n");
     x = 6;
     print!("The value of x is {x}\n");
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    print!("The value of this is {THREE_HOURS_IN_SECONDS}\n");
 }
