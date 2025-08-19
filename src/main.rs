6eaebcbdfcf7819489445c1294006fe6e09416df
solairerove <solairerove@gmail.com>
2025-08-19 10:19:27 +0700

    statement and expression

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
 fn main() {
-    print_labeled_measurement(5, 'h')
-}
-
-fn print_labeled_measurement(value: i32, unit_label: char) {
-    println!("The measurement is {value}{unit_label}");
+    let x = let y = 6;
 }
