28a0c335adf1b05218d31ef3521f166fa81192e3
solairerove <solairerove@gmail.com>
2025-08-19 23:07:00 +0700

    just playing with dumb fibonacci without handling 0 and 1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
-    for number in (1..4).rev() {
-        println!("{}", number);
+    let mut x = 0;
+    let mut y = 1;
+    let nth_fibonacci_number = 6;
+
+    for i in 0..nth_fibonacci_number {
+        let temp = x;
+        x = y;
+        y = y + temp;
     }
 
-    print!("LIFTOFF!!!");
+    print!("{y}");
 }
+// 0 1 1 2 3 5 8 13 21
