b884cbb13daf1cf03d74da22b4845bc582cd00ad
solairerove <solairerove@gmail.com>
2025-08-19 11:11:38 +0700

    rust reinvented while

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,10 @@
 fn main() {
-    let mut counter = 0;
+    let mut counter = 3;
 
-    'counting_loop: loop {
-        println!("Counter: {}", counter);
-        let mut remaining = 10;
-
-        loop {
-            println!("Remaining: {}", remaining);
-            if remaining == 9 {
-                break;
-            }
-
-            if counter == 2 {
-                break 'counting_loop;
-            }
-
-            remaining -= 1;
-        }
-
-        counter += 1;
+    while counter != 0 {
+        println!("{}", counter);
+        counter = counter - 1;
     }
 
-    println!("End of counter: {}", counter);
+    println!("LIFTOFF!!!");
 }
