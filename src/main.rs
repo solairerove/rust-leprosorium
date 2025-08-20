3d3aaa430b80df8962bcd592550dcb56db363ec0
solairerove <solairerove@gmail.com>
2025-08-21 00:29:47 +0700

    type passed to method as parameter takes ownership, gezes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 fn main() {
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
-    println!("{}, world!", s1);
-    println!("{}, world!", s2);
+    let s = String::from("hello");
+    takes_ownership(s.clone());
+    let y = s;
+    println!("{}", y);
+    let x = 5;
+    makes_copy(x);
+    let y = x;
+    println!("{}", y);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{some_string}");
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{some_integer}");
 }
