eb215d362cff2ab65b9d443f7022816f8b9f0510
solairerove <solairerove@gmail.com>
2025-08-21 00:35:52 +0700

    Transferring ownership of return values

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 fn main() {
-    let s = String::from("hello");
-    takes_ownership(s.clone());
-    let y = s;
-    println!("{}", y);
-    let x = 5;
-    makes_copy(x);
-    let y = x;
-    println!("{}", y);
+    let s1 = gives_ownership();
+    let s2 = String::from("hello");
+    // println!("{s2}");
+    let s3 = takes_and_gives_back(s1);
+
+    // println!("{s1}");,
+
+    println!("{s3}");
 }
 
-fn takes_ownership(some_string: String) {
-    println!("{some_string}");
+fn gives_ownership() -> String {
+    let some_string = String::from("give ownership");
+
+    some_string
 }
 
-fn makes_copy(some_integer: i32) {
-    println!("{some_integer}");
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }
