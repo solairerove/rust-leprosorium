a7552b23bb721a57edb1ab347eca693c0c7de776
solairerove <solairerove@gmail.com>
2025-08-21 00:19:00 +0700

    shallow copy

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
-    let mut x = 2;
-    let y = x;
-    println!("{x} {y}");
-    x = 4;
-    println!("{x} {y}");
+    let s1 = String::from("hello");
+    let s2 = s1;
+
+    println!("{}, world!", s2);
 }
