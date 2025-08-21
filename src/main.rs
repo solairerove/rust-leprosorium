c0338659aaff4ed7b528397445f172ea1680a3cc
solairerove <solairerove@gmail.com>
2025-08-21 10:04:34 +0700

    still strange this ownership

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ fn main() {
 
     let r1 = &s1;
     let r2 = &s1;
-    let r3 = &mut s1;
+    println!("{}, {}", r1, r2);
+    // let r4 = r1;
 
-    println!("{}, {}, {}", r1, r2, r3);
+    let r3 = &mut s1;
+    println!("{}", r3);
+
+    // println!("{}", r4);
 }
