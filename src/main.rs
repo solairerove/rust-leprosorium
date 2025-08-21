3c04af6d2039006e2ccbd0132c51caba8ece961f
solairerove <solairerove@gmail.com>
2025-08-21 09:56:01 +0700

    second mutable borrow occurs here

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 fn main() {
     let mut s1 = String::from("hello");
 
-    change(&mut s1);
+    let r1 = &mut s1;
+    let r2 = &mut s1;
 
-    println!("{}", s1);
-}
-
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+    println!("{}, {}", r1, r2);
 }
