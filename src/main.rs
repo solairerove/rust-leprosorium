612708836c5db1b87894e30f8ea623029890151b
solairerove <solairerove@gmail.com>
2025-08-21 23:23:49 +0700

    dot notation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
-    let user = User {
+    let mut user = User {
         active: true,
         username: String::from("solairerove"),
         email: String::from("solairerove@gmail.com"),
         sign_in_count: 1,
     };
+
+    println!("{:?}", user.sign_in_count);
+
+    user.sign_in_count = user.sign_in_count + 1;
 
     println!("{:?}", user.sign_in_count);
 }
