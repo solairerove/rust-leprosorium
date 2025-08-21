9a1554b518843f6e63df1a5fa2735f94613241ce
solairerove <solairerove@gmail.com>
2025-08-21 09:59:11 +0700

    scope has own ownership for mutable reference the same way as for value

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
     let mut s1 = String::from("hello");
 
-    let r1 = &mut s1;
+    {
+        let r1 = &mut s1;
+
+        println!("{}", r1);
+    }
+
     let r2 = &mut s1;
 
-    println!("{}, {}", r1, r2);
+    println!("{}", r2);
 }
