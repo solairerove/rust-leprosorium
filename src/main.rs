3431f3810e3fcd684f9fe89ed1de4a7c9577a31a
solairerove <solairerove@gmail.com>
2025-08-21 23:55:42 +0700

    unit like struct without any fields is like enum without any fields?

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 fn main() {
-    let black = Color(0, 0, 0);
-    let origin = Point(0, 0, 0);
-
-    println!("black[0] {}", black.0);
-    println!("origin[2] {}", origin.2);
-
-    let Point(x, y, z) = origin;
-    println!("x: {}, y: {}, z: {}", x, y, z);
+    let _always_equal = AlwaysEqual;
 }
 
-struct Color(i32, i32, i32);
-struct Point(i32, i32, i32);
+struct AlwaysEqual;
