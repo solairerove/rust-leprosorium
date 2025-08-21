b3f5e026db70ab62aee915903b9842d350f8e3d4
solairerove <solairerove@gmail.com>
2025-08-21 23:46:04 +0700

    tuple struts

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,13 @@
 fn main() {
-    let mut user = User {
-        active: true,
-        username: String::from("solairerove"),
-        email: String::from("solairerove@gmail.com"),
-        sign_in_count: 1,
-    };
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 
-    println!("{:?}", user.sign_in_count);
+    println!("black[0] {}", black.0);
+    println!("origin[2] {}", origin.2);
 
-    user.sign_in_count = user.sign_in_count + 1;
-
-    println!("{:?}", user.sign_in_count);
-
-    let mut user1 = build_user(String::from("pizda@gmail.com"), String::from("hui"));
-
-    println!("email: {} username: {}", user1.email, user1.username);
-
-    let user2 = User {
-        active: false,
-        ..user1
-    };
-
-    user1.sign_in_count = user.sign_in_count + 1;
-
-    println!("{:?} {}", user2.active, user2.username);
-
-    println!("{:?} {}", user1.active, user1.username);
+    let Point(x, y, z) = origin;
+    println!("x: {}, y: {}, z: {}", x, y, z);
 }
 
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        username,
-        email,
-        sign_in_count: 1,
-    }
-}
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
