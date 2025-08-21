1af1735e500c2d78f650e70011b2e006fbae53a7
solairerove <solairerove@gmail.com>
2025-08-21 09:52:49 +0700

    even references can be mutable?

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     let s1 = String::from("hello");
 
-    let len = calculate_length(&s1);
+    change(&s1);
 
-    println!("The length of '{}' is {}.", s1, len);
+    println!("{}", s1);
 }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+fn change(some_string: &String) {
+    some_string.push_str(", world");
 }
