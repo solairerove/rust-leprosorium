f16c5d680512c5e807233dd8e9ee832a0c311617
solairerove <solairerove@gmail.com>
2025-08-21 10:01:17 +0700

    mutable and immutable borrow

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 fn main() {
     let mut s1 = String::from("hello");
 
-    {
-        let r1 = &mut s1;
+    let r1 = &s1;
+    let r2 = &s1;
+    let r3 = &mut s1;
 
-        println!("{}", r1);
-    }
-
-    let r2 = &mut s1;
-
-    println!("{}", r2);
+    println!("{}, {}, {}", r1, r2, r3);
 }
