2837aff9cc5c405dba160fd63f4ad4cd236b4cd3
solairerove <solairerove@gmail.com>
2025-08-22 23:27:44 +0700

    oh wow, rust reinvented constructor

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,13 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -27,4 +34,7 @@ fn main() {
 
     println!("Can rect1 hold rect2 ? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3 ? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(30);
+    println!("Square: {:?}", square);
 }
