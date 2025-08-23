010e585ac31f41ea80d705831aad49d2de8559f0
solairerove <solairerove@gmail.com>
2025-08-23 21:27:49 +0700

    interesting way to handle optional using match

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,12 @@
 fn main() {
-    println!("{}", value_in_cents(Coin::Penny));
-    println!("{}", value_in_cents(Coin::Nickel));
-    println!("{}", value_in_cents(Coin::Dime));
-    println!("{}", value_in_cents(Coin::Quarter(UsState::Alabama)));
-    println!("{}", value_in_cents(Coin::Quarter(UsState::Alaska)));
+    let x = 32;
+    let _thirty_three = plus_one(Option::Some(x));
+    plus_one(Option::None);
 }
 
-#[derive(Debug)]
-enum UsState {
-    Alabama,
-    Alaska,
-    // --snip--
-}
-
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}!", state);
-            25
-        }
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        Some(i) => Some(i + 1),
+        None => None,
     }
 }
