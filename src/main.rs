68be0f3abc07654f0088bd70621049583c1ae3c7
solairerove <solairerove@gmail.com>
2025-08-23 17:41:22 +0700

    enum with no fields

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,10 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-
-    fn square(size: u32) -> Self {
-        Self {
-            width: size,
-            height: size,
-        }
-    }
+enum IpAddrKind {
+    V4,
+    V6,
 }
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
-    };
-
-    println!("Can rect1 hold rect2 ? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3 ? {}", rect1.can_hold(&rect3));
-
-    let square = Rectangle::square(30);
-    println!("Square: {:?}", square);
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    
 }
