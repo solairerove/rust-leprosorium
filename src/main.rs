f18a40e3e256ba7ba16ef545f8b60dccd959e524
solairerove <solairerove@gmail.com>
2025-08-23 17:52:53 +0700

    enum as with field, okay

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
-enum IpAddrKind {
-    V4,
-    V6,
-}
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
 
 fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-
-    println!("{}", home.address);
-    println!("{}", loopback.address);
+    let _home = IpAddr::V4(String::from("127.0.0.1"));
+    let _loopback = IpAddr::V6(String::from("::1"));
 }
