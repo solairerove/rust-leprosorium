81a57fa055ca89a94c7a621ac096be3dbb5cf50b
solairerove <solairerove@gmail.com>
2025-08-23 18:02:18 +0700

    enum as with fields of different types?!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 enum IpAddr {
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String),
 }
 
 fn main() {
-    let _home = IpAddr::V4(String::from("127.0.0.1"));
+    let _home = IpAddr::V4(127, 0, 0, 1);
     let _loopback = IpAddr::V6(String::from("::1"));
 }
