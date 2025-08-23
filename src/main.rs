5f0ca4373890b7c6d8dd0bb5c139a5e28dce9097
solairerove <solairerove@gmail.com>
2025-08-23 21:21:29 +0700

    it is not required to have all same fields in enums, makes code cleaner

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
+fn main() {
+    println!("{}", value_in_cents(Coin::Penny));
+    println!("{}", value_in_cents(Coin::Nickel));
+    println!("{}", value_in_cents(Coin::Dime));
+    println!("{}", value_in_cents(Coin::Quarter(UsState::Alabama)));
+    println!("{}", value_in_cents(Coin::Quarter(UsState::Alaska)));
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        },
+        Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
-}
-
-fn main() {
-    println!("{}", value_in_cents(Coin::Penny));
-    println!("{}", value_in_cents(Coin::Quarter));
 }
