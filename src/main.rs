da94caca5797d36115ad11f572490bdfeb7e97a7
solairerove <solairerove@gmail.com>
2025-08-23 21:38:32 +0700

    still switch case, nice

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
-    let x = 32;
-    let _thirty_three = plus_one(Option::Some(x));
-    plus_one(Option::None);
-}
-
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        Some(i) => Some(i + 1),
-        None => None,
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => reroll(),
     }
 }
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn reroll() {}
