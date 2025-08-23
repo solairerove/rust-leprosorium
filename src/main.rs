a3dd0ba7eeb5c0f555b8b216c8ca25bd61f5f81b
solairerove <solairerove@gmail.com>
2025-08-23 19:37:35 +0700

    match case is like switch case

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
 
 fn main() {
-    let _home = IpAddr::V4(127, 0, 0, 1);
-    let _loopback = IpAddr::V6(String::from("::1"));
+    println!("{}", value_in_cents(Coin::Penny));
+    println!("{}", value_in_cents(Coin::Quarter));
 }
