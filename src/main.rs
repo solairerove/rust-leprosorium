52e9e05905f6692f6debe9b276d24ea00c2234e7
solairerove <solairerove@gmail.com>
2025-08-23 17:46:28 +0700

    enum as a field type in another struct

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,22 @@ enum IpAddrKind {
     V6,
 }
 
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-    
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    println!("{}", home.address);
+    println!("{}", loopback.address);
 }
