ee6d96f4886c8cc210c527b41eb2e320f5f584ee
solairerove <solairerove@gmail.com>
2025-08-23 21:39:00 +0700

    you can even do nothing on not matching case

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@ fn main() {
     match dice_roll {
         3 => add_fancy_hat(),
         7 => remove_fancy_hat(),
-        _ => reroll(),
+        _ => (),
     }
 }
 
 fn add_fancy_hat() {}
 fn remove_fancy_hat() {}
-fn reroll() {}
