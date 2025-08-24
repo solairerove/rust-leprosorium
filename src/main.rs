291cf7115aee429246cf030445ab5538e7bc55cd
solairerove <solairerove@gmail.com>
2025-08-24 14:50:19 +0700

    this is how you can handle optional with match case

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 fn main() {
-    let dice_roll = 9;
-    match dice_roll {
-        3 => add_fancy_hat(),
-        7 => remove_fancy_hat(),
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("Max is {}", max),
         _ => (),
     }
 }
-
-fn add_fancy_hat() {}
-fn remove_fancy_hat() {}
