a3afb8bbdff27dca6c83ae2efa44638fe4bfdad7
solairerove <solairerove@gmail.com>
2025-08-26 12:47:23 +0700

    really, reinvented private and public access modifiers

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
 mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
     pub struct Breakfast {
         pub toast: String,
         seasonal_fruit: String,
@@ -18,4 +23,9 @@ pub fn eat_at_restaurant() {
     let mut meal = back_of_house::Breakfast::summer("Rye");
     meal.toast = String::from("Wheat");
     println!("I'd like {} toast please", meal.toast);
+
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let _order1 = back_of_house::Appetizer::Soup;
+    let _order2 = back_of_house::Appetizer::Salad;
 }
