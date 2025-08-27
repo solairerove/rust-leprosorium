6cf6da50e99019b1ee9e3a7a6c391af3988d407b
solairerove <solairerove@gmail.com>
2025-08-27 23:28:11 +0700

    hash map is owner for owned values like String, for i32 values are copied

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
 use std::collections::HashMap;
 
 fn main() {
-    let mut scores = HashMap::new();
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
-    println!("{:?}", scores);
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
 
-    let team_name = String::from("Blue");
-    let score = scores.get(&team_name);
-    match score {
-        Some(score) => println!("{}", score),
-        None => println!("No score"),
-    }
-
-    for (key, value) in &scores {
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    for (key, value) in &map {
         println!("{}: {}", key, value);
     }
 }
