06183c0cee009dba78f5b90d6dc21416efe1ac91
solairerove <solairerove@gmail.com>
2025-08-27 22:44:59 +0700

    string concat

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    let mut s1 = String::from("foo");
-    let s2 = "bar";
-    s1.push_str(s2);
-    println!("s1 is {}", s1);
-    println!("s2 is {}", s2);
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{s1}-{s2}-{s3}");
+    println!("{}", s);
 }
