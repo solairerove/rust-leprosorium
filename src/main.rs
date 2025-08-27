ebe2cc0c3f8e592602cd46c5466997f9eba99681
solairerove <solairerove@gmail.com>
2025-08-27 23:11:46 +0700

    hash map returns optional

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,8 @@ fn main() {
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
     println!("{:?}", scores);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).cloned().unwrap_or(0);
+    println!("{:?}", score);
 }
