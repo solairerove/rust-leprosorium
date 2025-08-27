8d53adb9892c35a1aa0ec06e1b83888892597ea1
solairerove <solairerove@gmail.com>
2025-08-27 23:06:44 +0700

    at least they have hash map

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::collections::HashMap;
+
 fn main() {
-    let hello = "Здравствуйте";
-    for c in hello.chars() {
-        println!("{}", c);
-    }
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    println!("{:?}", scores);
 }
