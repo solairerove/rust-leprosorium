31f5bfaeebba1008b4cf112a7692191223495c34
solairerove <solairerove@gmail.com>
2025-08-28 14:24:16 +0700

    more syntax sugar with closures

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,15 @@ use std::io::ErrorKind;
 fn main() {
     let greeting_file_result = File::open("hello.txt");
 
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("{:?}", e),
-            },
-            _ => {
-                panic!("Problem opening the file: {:?}", error);
-            }
+    let greeting_file = greeting_file_result.unwrap_or_else(|error| match error.kind() {
+        ErrorKind::NotFound => match File::create("hello.txt") {
+            Ok(fc) => fc,
+            Err(e) => panic!("{:?}", e),
         },
-    };
+        _ => {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
 
     println!("{}", greeting_file.metadata().unwrap().len());
 }
