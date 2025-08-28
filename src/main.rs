466ba0c6fff95d4e2de66beb319345166bdbb107
solairerove <solairerove@gmail.com>
2025-08-28 15:51:56 +0700

    shortcut for propagating errors with ? operator

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,9 @@ fn main() {
 }
 
 fn read_username_from_file() -> Result<String, std::io::Error> {
-    let username_file_result = File::open("username.txt");
+    let mut username_file = File::open("username.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
 
-    let mut username_file = match username_file_result {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-
-    let mut username: String = String::new();
-
-    match username_file.read_to_string(&mut username) {
-        Ok(_) => Ok(username),
-        Err(e) => Err(e),
-    }
+    Ok(username)
 }
