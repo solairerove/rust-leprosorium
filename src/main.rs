725d7e0af67231147eda60a23550ea7a1c5d291a
solairerove <solairerove@gmail.com>
2025-08-28 15:32:48 +0700

    propagating errors, btw it's easy to read from file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
 use std::fs::File;
+use std::io::Read;
 
 fn main() {
-    let greeting_file = File::open("hello.txt").expect("File not found");
+    let res = read_username_from_file();
+    match res {
+        Ok(username) => println!("{}", username),
+        Err(err) => println!("{}", err),
+    }
+}
+
+fn read_username_from_file() -> Result<String, std::io::Error> {
+    let username_file_result = File::open("username.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username: String = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
 }
