60362e0caf90fd38b4be40bf8f4648f9a4107d60
solairerove <solairerove@gmail.com>
2025-08-28 14:26:29 +0700

    expect and unwrap methods as wrappers for match case

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
 use std::fs::File;
-use std::io::ErrorKind;
 
 fn main() {
-    let greeting_file_result = File::open("hello.txt");
-
-    let greeting_file = greeting_file_result.unwrap_or_else(|error| match error.kind() {
-        ErrorKind::NotFound => match File::create("hello.txt") {
-            Ok(fc) => fc,
-            Err(e) => panic!("{:?}", e),
-        },
-        _ => {
-            panic!("Problem opening the file: {:?}", error);
-        }
-    });
-
-    println!("{}", greeting_file.metadata().unwrap().len());
+    let greeting_file = File::open("hello.txt").expect("File not found");
 }
