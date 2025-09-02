b2f89f35bd9d4aa493893ca281543a401e567ddf
solairerove <solairerove@gmail.com>
2025-09-02 23:39:43 +0700

    can use generics for structs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let integer = Point { x: 1, y: 2 };
+    let float = Point { x: 5.0, y: 6.0 };
 
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {result}");
+    println!("Point is at ({}, {})", integer.x, integer.y);
+    println!("Point is at ({}, {})", float.x, float.y);
 }
 
-fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
-    let mut largest = &list[0];
-
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
+struct Point<T> {
+    x: T,
+    y: T,
 }
