0730d3c30d0017a6dd3e92d3f229a620e6c20235
solairerove <solairerove@gmail.com>
2025-09-04 13:50:28 +0700

    default method in trait works perfectly fine

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,24 @@ fn main() {
     };
 
     println!("1 new post: {}", post.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
 }
 
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
 }
 
 pub struct NewsArticle {
@@ -20,11 +34,7 @@ pub struct NewsArticle {
     pub content: String,
 }
 
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
-    }
-}
+impl Summary for NewsArticle {}
 
 pub struct SocialPost {
     pub username: String,
