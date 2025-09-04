a961c5747f905aafdc5198f800a7c682f9d8b38b
solairerove <solairerove@gmail.com>
2025-09-04 19:39:31 +0700

    brainfuck, generic type params, trait bounds and lifetimes together

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-fn main() {
-    let string1 = String::from("abcd");
-    let result;
-    {
-        let string2 = String::from("xyz");
+use std::fmt::Display;
 
-        result = longest(string1.as_str(), string2.as_str());
-    }
-    println!("The longest string is {result}");
-}
+fn main() {}
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {ann}");
     if x.len() > y.len() { x } else { y }
 }
