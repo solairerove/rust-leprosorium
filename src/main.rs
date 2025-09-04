a2eec064b32f4c6dcb57b376bc33f59593c6c89a
solairerove <solairerove@gmail.com>
2025-09-04 19:22:05 +0700

    why? "Lifetime Annotations in Function Signatures"

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,15 @@
-use std::fmt::Display;
-
 fn main() {
-    let point = Pair { x: 12, y: 21 };
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
 
-    let another_one = Pair {
-        x: String::from("x"),
-        y: String::from("y"),
-    };
-
-    point.cmp_display();
-    another_one.cmp_display()
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {result}");
 }
 
-struct Pair<T> {
-    x: T,
-    y: T,
-}
-
-impl<T: Display + PartialOrd> Pair<T> {
-    fn cmp_display(&self) {
-        if self.x >= self.y {
-            println!("The largest member is x = {}", self.x);
-        } else {
-            println!("The largest member is y = {}", self.y);
-        }
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
 }
