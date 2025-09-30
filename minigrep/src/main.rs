f9e85cc5038d0a18dd73b9b0ed48ef54d9bba757
solairerove <solairerove@gmail.com>
2025-09-30 12:13:20 +0700

    Splitting Code into a Library Crate

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,4 @@
+use minigrep::search;
 use std::error::Error;
 use std::{env, fs, process};
 
@@ -8,9 +9,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for '{}'", config.query);
-    println!("In file '{}'", config.file_path);
-
     if let Err(e) = run(config) {
         println!("Application error: {e}");
         process::exit(1);
@@ -20,7 +18,9 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("\nWith text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
